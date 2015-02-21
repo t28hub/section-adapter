10887ff87238bfcb4bdd1ea792c4974bfc9e0b08
Tatsuya Maki <t28oogle@gmail.com>
2015-02-22 02:47:00 +0900

    dras(Vertical|Horizontal)の追加

--- sample/src/main/java/com/t28/sectionadapter/sample/DividerItemDecoration.java
+++ sample/src/main/java/com/t28/sectionadapter/sample/DividerItemDecoration.java
@@ -1,5 +1,6 @@
 package com.t28.sectionadapter.sample;
 
+import android.graphics.Canvas;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
@@ -16,7 +17,22 @@ public class DividerItemDecoration extends RecyclerView.ItemDecoration {
     }
 
     @Override
+    public void onDraw(Canvas canvas, RecyclerView parent, RecyclerView.State state) {
+        if (parent.getChildCount() == 0) {
+            return;
+        }
+        drawVertical(canvas, parent);
+        drawHorizontal(canvas, parent);
+    }
+
+    @Override
     public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
         outRect.set(0, 0, mDivider.getIntrinsicWidth(), mDivider.getIntrinsicHeight());
+    }
+
+    private void drawVertical(Canvas canvas, RecyclerView parent) {
+    }
+
+    private void drawHorizontal(Canvas canvas, RecyclerView parent) {
     }
 }
