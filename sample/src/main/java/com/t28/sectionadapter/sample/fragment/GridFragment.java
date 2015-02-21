b3f7d529378d7f47dea2554259c51cd2793f29f2
Tatsuya Maki <t28oogle@gmail.com>
2015-02-22 01:11:55 +0900

    GridFragment$Creatorの追加

--- sample/src/main/java/com/t28/sectionadapter/sample/fragment/GridFragment.java
+++ sample/src/main/java/com/t28/sectionadapter/sample/fragment/GridFragment.java
@@ -2,7 +2,17 @@ package com.t28.sectionadapter.sample.fragment;
 
 import android.support.v4.app.Fragment;
 
+import com.t28.sectionadapter.sample.FragmentAdapter;
+
 public class GridFragment extends Fragment {
     public GridFragment() {
+    }
+
+    public static class Creator implements FragmentAdapter.FragmentCreator {
+
+        @Override
+        public Fragment create() {
+            return new GridFragment();
+        }
     }
 }
