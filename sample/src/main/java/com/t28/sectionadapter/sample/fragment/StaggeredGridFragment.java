56d8e00cff48487c66cc00f2d05d0397f9072b6b
Tatsuya Maki <t28oogle@gmail.com>
2015-02-22 01:10:25 +0900

    StaggeredGridFragment$Creator追加

--- sample/src/main/java/com/t28/sectionadapter/sample/fragment/StaggeredGridFragment.java
+++ sample/src/main/java/com/t28/sectionadapter/sample/fragment/StaggeredGridFragment.java
@@ -2,7 +2,17 @@ package com.t28.sectionadapter.sample.fragment;
 
 import android.support.v4.app.Fragment;
 
+import com.t28.sectionadapter.sample.FragmentAdapter;
+
 public class StaggeredGridFragment extends Fragment {
     public StaggeredGridFragment() {
+    }
+
+    public static class Creator implements FragmentAdapter.FragmentCreator {
+
+        @Override
+        public Fragment create() {
+            return new StaggeredGridFragment();
+        }
     }
 }
