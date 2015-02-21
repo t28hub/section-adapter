ee6ba9cfecfc262437d33344fd4224b087b46dd5
Tatsuya Maki <t28oogle@gmail.com>
2015-02-21 22:10:21 +0900

    MainFragmentのルートレイアウトをRecyclerView化

--- sample/src/main/java/com/t28/sectionadapter/sample/fragment/MainFragment.java
+++ sample/src/main/java/com/t28/sectionadapter/sample/fragment/MainFragment.java
@@ -2,6 +2,8 @@ package com.t28.sectionadapter.sample.fragment;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +11,8 @@ import android.view.ViewGroup;
 import com.t28.sectionadapter.sample.R;
 
 public class MainFragment extends Fragment {
+    private static final int SPAN_COUNT = 2;
+
     public MainFragment() {
     }
 
@@ -18,6 +22,9 @@ public class MainFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final RecyclerView listView = (RecyclerView) inflater.inflate(R.layout.fragment_main, container, false);
+        listView.setHasFixedSize(true);
+        listView.setLayoutManager(new GridLayoutManager(getActivity(), SPAN_COUNT));
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
