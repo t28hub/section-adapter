c59f77ed4c0d4e6f46b3ba13932e1209b9c8c896
Tatsuya Maki <t28oogle@gmail.com>
2015-02-22 02:43:20 +0900

    DividerItemDecoration#getItemOffsetsの実装

--- sample/src/main/java/com/t28/sectionadapter/sample/DividerItemDecoration.java
+++ sample/src/main/java/com/t28/sectionadapter/sample/DividerItemDecoration.java
@@ -1,7 +1,9 @@
 package com.t28.sectionadapter.sample;
 
+import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 public class DividerItemDecoration extends RecyclerView.ItemDecoration {
     private final Drawable mDivider;
@@ -13,4 +15,8 @@ public class DividerItemDecoration extends RecyclerView.ItemDecoration {
         mDivider = divider;
     }
 
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+        outRect.set(0, 0, mDivider.getIntrinsicWidth(), mDivider.getIntrinsicHeight());
+    }
 }
