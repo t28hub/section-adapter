8f385fc27b07d62fbe787fbc2e70925bc2d02fc4
Tatsuya Maki <t28oogle@gmail.com>
2015-02-21 19:08:00 +0900

    SectionAdapterの追加

---
+++ library/src/main/java/com/t28/sectionadapter/SectionAdapter.java
@@ -0,0 +1,21 @@
+package com.t28.sectionadapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class SectionAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
