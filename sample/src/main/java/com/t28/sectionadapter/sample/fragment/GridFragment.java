8134c15b0c08bbbb8f9641bc443eae0268ae8021
Tatsuya Maki <t28oogle@gmail.com>
2015-02-22 01:19:17 +0900

    onCreateViewを実装

--- sample/src/main/java/com/t28/sectionadapter/sample/fragment/GridFragment.java
+++ sample/src/main/java/com/t28/sectionadapter/sample/fragment/GridFragment.java
@@ -1,11 +1,21 @@
 package com.t28.sectionadapter.sample.fragment;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.t28.sectionadapter.sample.FragmentAdapter;
+import com.t28.sectionadapter.sample.R;
 
 public class GridFragment extends Fragment {
     public GridFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_grid, container, false);
     }
 
     public static class Creator implements FragmentAdapter.FragmentCreator {
--- sample/src/main/java/com/t28/sectionadapter/sample/fragment/LinearFragment.java
+++ sample/src/main/java/com/t28/sectionadapter/sample/fragment/LinearFragment.java
@@ -1,11 +1,21 @@
 package com.t28.sectionadapter.sample.fragment;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.t28.sectionadapter.sample.FragmentAdapter;
+import com.t28.sectionadapter.sample.R;
 
 public class LinearFragment extends Fragment {
     public LinearFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_linear, container, false);
     }
 
     public static class Creator implements FragmentAdapter.FragmentCreator {
