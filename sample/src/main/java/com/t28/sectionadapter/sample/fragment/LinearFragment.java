c58c3df80f92a36a59e97f87843b02e749968cfb
Tatsuya Maki <t28oogle@gmail.com>
2015-02-22 01:09:05 +0900

    XxxFragmentの追加

---
+++ sample/src/main/java/com/t28/sectionadapter/sample/fragment/LinearFragment.java
@@ -0,0 +1,8 @@
+package com.t28.sectionadapter.sample.fragment;
+
+import android.support.v4.app.Fragment;
+
+public class LinearFragment extends Fragment {
+    public LinearFragment() {
+    }
+}
---
+++ sample/src/main/java/com/t28/sectionadapter/sample/fragment/StaggeredGridFragment.java
@@ -0,0 +1,8 @@
+package com.t28.sectionadapter.sample.fragment;
+
+import android.support.v4.app.Fragment;
+
+public class StaggeredGridFragment extends Fragment {
+    public StaggeredGridFragment() {
+    }
+}
