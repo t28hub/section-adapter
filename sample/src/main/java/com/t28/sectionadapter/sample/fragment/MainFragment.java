8ab88210967c9fe703c177d167a08f32b7e91edc
Tatsuya Maki <t28oogle@gmail.com>
2015-02-22 01:22:30 +0900

    PagerAdapterの実装

--- sample/src/main/java/com/t28/sectionadapter/sample/fragment/MainFragment.java
+++ sample/src/main/java/com/t28/sectionadapter/sample/fragment/MainFragment.java
@@ -1,13 +1,18 @@
 package com.t28.sectionadapter.sample.fragment;
 
-import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.t28.sectionadapter.sample.FragmentAdapter;
 import com.t28.sectionadapter.sample.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainFragment extends Fragment {
     public MainFragment() {
@@ -20,7 +25,15 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final ViewPager pager = (ViewPager) inflater.inflate(R.layout.fragment_main, container, false);
-        pager.setAdapter(null);
+        pager.setAdapter(createPagerAdapter());
         return pager;
+    }
+
+    private PagerAdapter createPagerAdapter() {
+        final List<FragmentAdapter.FragmentCreator> creators = new ArrayList<>();
+        creators.add(new LinearFragment.Creator());
+        creators.add(new GridFragment.Creator());
+        creators.add(new StaggeredGridFragment.Creator());
+        return new FragmentAdapter(getChildFragmentManager(), creators);
     }
 }
