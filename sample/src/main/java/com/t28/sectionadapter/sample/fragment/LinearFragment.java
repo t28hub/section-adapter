5abe7f8ce0dd990e580b4785a5c1bb1efd20e25f
Tatsuya Maki <t28oogle@gmail.com>
2015-02-22 02:24:50 +0900

    ダミーデータ追加

--- sample/src/main/java/com/t28/sectionadapter/sample/fragment/LinearFragment.java
+++ sample/src/main/java/com/t28/sectionadapter/sample/fragment/LinearFragment.java
@@ -2,12 +2,16 @@ package com.t28.sectionadapter.sample.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.t28.sectionadapter.sample.adapter.FragmentAdapter;
 import com.t28.sectionadapter.sample.R;
+import com.t28.sectionadapter.sample.adapter.ItemAdapter;
+import com.t28.sectionadapter.sample.adapter.SimpleSectionAdapter;
 
 public class LinearFragment extends Fragment {
     public LinearFragment() {
@@ -15,7 +19,25 @@ public class LinearFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_linear, container, false);
+        final RecyclerView view = (RecyclerView) inflater.inflate(R.layout.fragment_linear, container, false);
+        view.setHasFixedSize(true);
+        view.setLayoutManager(new LinearLayoutManager(getActivity()));
+        view.setAdapter(createAdapter());
+        return view;
+    }
+
+    private RecyclerView.Adapter createAdapter() {
+        final SimpleSectionAdapter adapter = new SimpleSectionAdapter();
+        adapter.append("Section1");
+        adapter.append("Section1-1", new ItemAdapter());
+        adapter.append("Section1-2", new ItemAdapter());
+        adapter.append("Section2");
+        adapter.append("Section2-1", new ItemAdapter());
+        adapter.append("Section2-2", new ItemAdapter());
+        adapter.append("Section2-3", new ItemAdapter());
+        adapter.append("Section3");
+        adapter.append("Section3-1", new ItemAdapter());
+        return adapter;
     }
 
     public static class Creator implements FragmentAdapter.FragmentCreator {
