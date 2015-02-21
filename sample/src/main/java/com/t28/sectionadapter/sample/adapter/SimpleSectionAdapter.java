f8aa6c295f57037fdd3777899e9258ff009bf7c1
Tatsuya Maki <t28oogle@gmail.com>
2015-02-21 22:36:33 +0900

    SimpleSectionAdapterを実装

--- sample/src/main/java/com/t28/sectionadapter/sample/adapter/SimpleSectionAdapter.java
+++ sample/src/main/java/com/t28/sectionadapter/sample/adapter/SimpleSectionAdapter.java
@@ -1,21 +1,26 @@
 package com.t28.sectionadapter.sample.adapter;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.t28.sectionadapter.SectionAdapter;
+import com.t28.sectionadapter.sample.R;
 
 public class SimpleSectionAdapter extends SectionAdapter<String, SimpleSectionAdapter.HeaderViewHolder> {
 
     @Override
     protected HeaderViewHolder onCreateHeaderHolder(ViewGroup parent, int viewType) {
-        return null;
+        final LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        final View headerView = inflater.inflate(R.layout.layout_main_header, parent, false);
+        return new HeaderViewHolder(headerView);
     }
 
     @Override
     protected void onBindHeaderHolder(HeaderViewHolder holder, int sectionPosition, String header) {
-
+        holder.bind(header);
     }
 
     @Override
@@ -25,13 +30,17 @@ public class SimpleSectionAdapter extends SectionAdapter<String, SimpleSectionAd
 
     @Override
     protected long getHeaderId(int sectionPosition, String header) {
-        return 0;
+        return header.hashCode();
     }
 
     class HeaderViewHolder extends RecyclerView.ViewHolder {
 
         public HeaderViewHolder(View itemView) {
             super(itemView);
+        }
+
+        public void bind(String header) {
+            ((TextView) itemView).setText(header);
         }
     }
 }
