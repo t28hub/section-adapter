f2ed6f603e02242034a5a146c846b055daeefbb1
Tatsuya Maki <t28oogle@gmail.com>
2015-02-21 19:10:31 +0900

    Sectionの追加

---
+++ library/src/main/java/com/t28/sectionadapter/Section.java
@@ -0,0 +1,13 @@
+package com.t28.sectionadapter;
+
+import android.support.v7.widget.RecyclerView;
+
+class Section<T> {
+    private final T mHeader;
+    private RecyclerView.Adapter mAdapter;
+
+    public Section(T header, RecyclerView.Adapter adapter) {
+        mHeader = header;
+        mAdapter = adapter;
+    }
+}
