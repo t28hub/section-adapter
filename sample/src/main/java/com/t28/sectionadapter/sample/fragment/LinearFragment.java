12876ce33e4ec6366d79012a29ed50830495b695
Tatsuya Maki <t28oogle@gmail.com>
2015-02-22 01:11:16 +0900

    LinearFragment$Creator追加

--- sample/src/main/java/com/t28/sectionadapter/sample/fragment/LinearFragment.java
+++ sample/src/main/java/com/t28/sectionadapter/sample/fragment/LinearFragment.java
@@ -2,7 +2,17 @@ package com.t28.sectionadapter.sample.fragment;
 
 import android.support.v4.app.Fragment;
 
+import com.t28.sectionadapter.sample.FragmentAdapter;
+
 public class LinearFragment extends Fragment {
     public LinearFragment() {
+    }
+
+    public static class Creator implements FragmentAdapter.FragmentCreator {
+
+        @Override
+        public Fragment create() {
+            return new LinearFragment();
+        }
     }
 }
