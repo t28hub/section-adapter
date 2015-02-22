be37b8dfc5e864234004f379b1fe32db12ba0414
Tatsuya Maki <t28oogle@gmail.com>
2015-02-22 16:27:42 +0900

    データ生成処理をDymmyDataSetに移動

--- sample/src/main/java/com/t28/sectionadapter/sample/fragment/LinearFragment.java
+++ sample/src/main/java/com/t28/sectionadapter/sample/fragment/LinearFragment.java
@@ -12,9 +12,8 @@ import android.view.ViewGroup;
 import com.t28.sectionadapter.sample.R;
 import com.t28.sectionadapter.sample.adapter.FragmentAdapter;
 import com.t28.sectionadapter.sample.adapter.SimpleSectionAdapter;
+import com.t28.sectionadapter.sample.data.DummyDataSet;
 
-import java.util.Arrays;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -33,33 +32,7 @@ public class LinearFragment extends Fragment {
 
     private RecyclerView.Adapter createAdapter() {
         final Resources resources = getResources();
-        final Map<String, List<String>> sections = new LinkedHashMap<>();
-
-        // Android 1.x
-        final String android1 = resources.getString(R.string.android_1);
-        final List<String> android1Names = Arrays.asList(resources.getStringArray(R.array.android_1_names));
-        sections.put(android1, android1Names);
-
-        // Android 2.x
-        final String android2 = resources.getString(R.string.android_2);
-        final List<String> android2Names = Arrays.asList(resources.getStringArray(R.array.android_2_names));
-        sections.put(android2, android2Names);
-
-        // Android 3.x
-        final String android3 = resources.getString(R.string.android_3);
-        final List<String> android3Names = Arrays.asList(resources.getStringArray(R.array.android_3_names));
-        sections.put(android3, android3Names);
-
-        // Android 4.x
-        final String android4 = resources.getString(R.string.android_4);
-        final List<String> android4Names = Arrays.asList(resources.getStringArray(R.array.android_4_names));
-        sections.put(android4, android4Names);
-
-        // Android 5.x
-        final String android5 = resources.getString(R.string.android_5);
-        final List<String> android5Names = Arrays.asList(resources.getStringArray(R.array.android_5_names));
-        sections.put(android5, android5Names);
-
+        final Map<String, List<String>> sections = DummyDataSet.versionNames(resources);
         return new SimpleSectionAdapter(sections);
     }
 
