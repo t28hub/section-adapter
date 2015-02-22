fb68e36e6137ec3a40c356a202632e04c10a4931
Tatsuya Maki <t28oogle@gmail.com>
2015-02-22 14:54:07 +0900

    Section2の実装を見直し

--- library/src/main/java/com/t28/sectionadapter/Section2.java
+++ library/src/main/java/com/t28/sectionadapter/Section2.java
@@ -2,25 +2,25 @@ package com.t28.sectionadapter;
 
 import android.support.v7.widget.RecyclerView;
 
-class Section2<T> {
+class Section2 {
     private static final Section2 EMPTY = new Section2();
 
-    private final T mHeader;
     private final RecyclerView.Adapter mAdapter;
     private int mHeaderPosition = RecyclerView.NO_POSITION;
 
-    Section2(T header, RecyclerView.Adapter adapter) {
-        mHeader = header;
+    Section2(RecyclerView.Adapter adapter) {
+        if (adapter == null) {
+            throw new NullPointerException("adapter == null");
+        }
         mAdapter = adapter;
     }
 
     private Section2() {
-        mHeader = null;
         mAdapter = null;
     }
 
     boolean isEmpty() {
-        return mHeader == null && mAdapter == null;
+        return mAdapter == null;
     }
 
     boolean isHeaderPosition(int position) {
@@ -31,14 +31,6 @@ class Section2<T> {
         final int positionStart = mHeaderPosition + 1;
         final int positionEnd = mHeaderPosition + mAdapter.getItemCount();
         return positionStart <= position && position <= positionEnd;
-    }
-
-    T getHeader() {
-        return mHeader;
-    }
-
-    RecyclerView.Adapter getAdapter() {
-        return mAdapter;
     }
 
     void setHeaderPosition(int position) {
@@ -53,8 +45,7 @@ class Section2<T> {
         return position - mHeaderPosition - 1;
     }
 
-    @SuppressWarnings("unchecked")
-    static <T> Section2<T> emptySection() {
+    static Section2 emptySection() {
         return EMPTY;
     }
 }
