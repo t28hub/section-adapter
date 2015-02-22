de1f418616aa517b41b48f2c4a010e12e42a9779
Tatsuya Maki <t28oogle@gmail.com>
2015-02-22 14:14:43 +0900

    SectionAdapterのIF変更版を追加

---
+++ library/src/main/java/com/t28/sectionadapter/SectionAdapter2.java
@@ -0,0 +1,21 @@
+package com.t28.sectionadapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class SectionAdapter2 extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
