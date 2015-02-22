5f68cc1329ea6ed1281269f00b113515423fd908
Tatsuya Maki <t28oogle@gmail.com>
2015-02-22 16:30:25 +0900

    GridFragmentをRecyclerView化

--- sample/src/main/java/com/t28/sectionadapter/sample/fragment/GridFragment.java
+++ sample/src/main/java/com/t28/sectionadapter/sample/fragment/GridFragment.java
@@ -1,13 +1,21 @@
 package com.t28.sectionadapter.sample.fragment;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.t28.sectionadapter.sample.adapter.FragmentAdapter;
 import com.t28.sectionadapter.sample.R;
+import com.t28.sectionadapter.sample.adapter.FragmentAdapter;
+import com.t28.sectionadapter.sample.adapter.SimpleSectionAdapter;
+import com.t28.sectionadapter.sample.data.DummyDataSet;
+
+import java.util.List;
+import java.util.Map;
 
 public class GridFragment extends Fragment {
     public GridFragment() {
@@ -15,7 +23,17 @@ public class GridFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_grid, container, false);
+        final RecyclerView view = (RecyclerView) inflater.inflate(R.layout.fragment_grid, container, false);
+        view.setHasFixedSize(true);
+        view.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        view.setAdapter(createAdapter());
+        return view;
+    }
+
+    private RecyclerView.Adapter createAdapter() {
+        final Resources resources = getResources();
+        final Map<String, List<String>> sections = DummyDataSet.versionNames(resources);
+        return new SimpleSectionAdapter(sections);
     }
 
     public static class Creator implements FragmentAdapter.FragmentCreator {
