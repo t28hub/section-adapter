bd3998d2655709732a502494e67e7d909596717e
Tatsuya Maki <t28oogle@gmail.com>
2015-02-22 14:24:16 +0900

    getItemAdapter追加

--- library/src/main/java/com/t28/sectionadapter/SectionAdapter2.java
+++ library/src/main/java/com/t28/sectionadapter/SectionAdapter2.java
@@ -3,7 +3,7 @@ package com.t28.sectionadapter;
 import android.support.v7.widget.RecyclerView;
 import android.view.ViewGroup;
 
-public abstract class SectionAdapter2<VH extends RecyclerView.ViewHolder> extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public abstract class SectionAdapter2<VH1 extends RecyclerView.ViewHolder, VH2 extends RecyclerView.ViewHolder> extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         return null;
@@ -19,13 +19,15 @@ public abstract class SectionAdapter2<VH extends RecyclerView.ViewHolder> extend
         return 0;
     }
 
-    protected abstract VH onCreateHaederViewHolder(ViewGroup parent, int viewType);
+    protected abstract VH1 onCreateHaederViewHolder(ViewGroup parent, int viewType);
 
-    protected abstract void onBindHeaderViewHolder(VH holder, int sectionPosition);
+    protected abstract void onBindHeaderViewHolder(VH1 holder, int sectionPosition);
 
     protected abstract int getHeaderViewType(int sectionPosition);
 
     protected abstract long getHeaderId(int sectionPosition);
 
     protected abstract int getHeaderCount();
+
+    protected abstract RecyclerView.Adapter<VH2> getItemAdapter(int sectionPosition);
 }
