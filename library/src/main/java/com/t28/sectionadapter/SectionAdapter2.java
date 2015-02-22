3c2c5896b789603732f84999173a8273f6c3941b
Tatsuya Maki <t28oogle@gmail.com>
2015-02-22 14:19:46 +0900

    Header関連の抽象メソッド追加

--- library/src/main/java/com/t28/sectionadapter/SectionAdapter2.java
+++ library/src/main/java/com/t28/sectionadapter/SectionAdapter2.java
@@ -3,7 +3,7 @@ package com.t28.sectionadapter;
 import android.support.v7.widget.RecyclerView;
 import android.view.ViewGroup;
 
-public class SectionAdapter2 extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public abstract class SectionAdapter2<VH extends RecyclerView.ViewHolder> extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         return null;
@@ -18,4 +18,14 @@ public class SectionAdapter2 extends RecyclerView.Adapter<RecyclerView.ViewHolde
     public int getItemCount() {
         return 0;
     }
+
+    protected abstract VH onCreateHaederViewHolder(ViewGroup parent, int viewType);
+
+    protected abstract void onBindHeaderViewHolder(VH holder, int sectionPosition);
+
+    protected abstract int getHeaderViewType(int sectionPosition);
+
+    protected abstract long getHeaderId(int sectionPosition);
+
+    protected abstract int getHeaderCount();
 }
