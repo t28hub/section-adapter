fb1b8c442fc0e2afffc3905c6c060076854677e2
Tatsuya Maki <t28oogle@gmail.com>
2015-02-22 14:33:23 +0900

    refreshSections実装

--- library/src/main/java/com/t28/sectionadapter/SectionAdapter2.java
+++ library/src/main/java/com/t28/sectionadapter/SectionAdapter2.java
@@ -3,7 +3,16 @@ package com.t28.sectionadapter;
 import android.support.v7.widget.RecyclerView;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class SectionAdapter2<VH1 extends RecyclerView.ViewHolder, VH2 extends RecyclerView.ViewHolder> extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private final List<Section<String>> mSections;
+
+    public SectionAdapter2() {
+        mSections = new ArrayList<>();
+    }
+
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         return null;
@@ -22,6 +31,21 @@ public abstract class SectionAdapter2<VH1 extends RecyclerView.ViewHolder, VH2 e
             itemCount += getItemAdapter(position).getItemCount();
         }
         return itemCount;
+    }
+
+    public void refreshSections() {
+        Section previous = Section.emptySection();
+        for (Section section : mSections) {
+            final int position;
+            if (previous.isEmpty()) {
+                position = 0;
+            } else {
+                position = previous.getHeaderPosition() + previous.getAdapter().getItemCount() + 1;
+            }
+            section.setHeaderPosition(position);
+            previous = section;
+        }
+        notifyDataSetChanged();
     }
 
     protected abstract VH1 onCreateHaederViewHolder(ViewGroup parent, int viewType);
